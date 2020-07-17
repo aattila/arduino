e8d1f7cb9508d222a61c3eb024685c0c25fe2e19
Attila Antal <attila.antal@accesa.eu>
2020-07-17 19:03:10 +0300

    reenabled eth

--- nano_mqtt_sub_terrace_2/nano_mqtt_sub_terrace_2.ino
+++ nano_mqtt_sub_terrace_2/nano_mqtt_sub_terrace_2.ino
@@ -123,50 +123,50 @@ void reconnect() {
 }
 
 void loop() {
-  //  if (!isEthUp or !client.connected() ) {
-  //    reconnect();
-  //  }
-
-  if ( (millis() - lastDebounceTime) > debounceDelay) {
-    data = pulseIn(IR_DATA, HIGH);
-    if (data > 0) {
-      Serial.print("PROC: ");
-      Serial.println(data);
-      if (data > 75 && data < 80 ) { //1
-        setRelay(0);
-      } else if (data > 150 && data < 160) { //2
-        setRelay(1);
-      } else if (data > 230 && data < 240) { //3
-        setRelay(2);
-      } else if (data > 305 && data < 315) { //4
-        setRelay(3);
-      } else if (data > 380 && data < 390) { //5
-        setRelay(4);
-      } else if (data > 460 && data < 470) { //6
-        setRelay(5);
-      } else if (data > 535 && data < 545) { //7
-        setRelay(6);
-      } else if (data > 615 && data < 625) { //8
-        setRelay(7);
-      } else if (data > 690 && data < 700) { //9
-      } else if (data > 770 && data < 780) { //0
-      } else if (data > 845 && data < 855) { //STAR
-        setStar();
-      } else if (data > 920 && data < 930) { //SHARP
-        setRelay(7);
-      } else if (data > 1001 && data < 1010) { //UP
-      } else if (data > 1080 && data < 1090) { //DOWN
-      } else if (data > 1155 && data < 1165) { //LEFT
-      } else if (data > 1230 && data < 1240) { //RIGHT
-      } else if (data > 1310 && data < 1320) { //OK
-        shutDown();
-      }
-      data = 0;
-    }
-    lastDebounceTime = millis();
-  } else {
-    data = 0;
+  if (!isEthUp or !client.connected() ) {
+    reconnect();
   }
 
-  //  client.loop();
+//  if ( (millis() - lastDebounceTime) > debounceDelay) {
+//    data = pulseIn(IR_DATA, HIGH);
+//    if (data > 0) {
+//      Serial.print("PROC: ");
+//      Serial.println(data);
+//      if (data > 75 && data < 80 ) { //1
+//        setRelay(0);
+//      } else if (data > 150 && data < 160) { //2
+//        setRelay(1);
+//      } else if (data > 230 && data < 240) { //3
+//        setRelay(2);
+//      } else if (data > 305 && data < 315) { //4
+//        setRelay(3);
+//      } else if (data > 380 && data < 390) { //5
+//        setRelay(4);
+//      } else if (data > 460 && data < 470) { //6
+//        setRelay(5);
+//      } else if (data > 535 && data < 545) { //7
+//        setRelay(6);
+//      } else if (data > 615 && data < 625) { //8
+//        setRelay(7);
+//      } else if (data > 690 && data < 700) { //9
+//      } else if (data > 770 && data < 780) { //0
+//      } else if (data > 845 && data < 855) { //STAR
+//        setStar();
+//      } else if (data > 920 && data < 930) { //SHARP
+//        setRelay(7);
+//      } else if (data > 1001 && data < 1010) { //UP
+//      } else if (data > 1080 && data < 1090) { //DOWN
+//      } else if (data > 1155 && data < 1165) { //LEFT
+//      } else if (data > 1230 && data < 1240) { //RIGHT
+//      } else if (data > 1310 && data < 1320) { //OK
+//        shutDown();
+//      }
+//      data = 0;
+//    }
+//    lastDebounceTime = millis();
+//  } else {
+//    data = 0;
+//  }
+
+  client.loop();
 }
